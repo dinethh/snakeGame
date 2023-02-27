5dc7dc62e0de3fcdaf21738d5db1d53d2978c2d6
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-02-27 15:35:28 +0530

    p_holder methord added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,12 +19,55 @@ public class Main {
 
         print_board(g_board);
     }
-    public static void print_board(char [][]g_board){
+
+    public static void print_board(char[][] g_board) {
         for (int r = 0; r < g_board.length; r++) {
             for (int c = 0; c < g_board[0].length; c++) {
                 System.out.print(g_board[r][c]);
             }
             System.out.println();
         }
+    }
+
+    static void p_holder(char[][] g_board, int pos, String user) {
+        char syb = 'x';
+        if (user.equals("You")) {
+            syb = 'x';
+        } else if (user.equals("Comp")) {
+            syb = '0';
+        }
+        switch (pos) {
+            case 1:
+                g_board[0][0] = syb;
+                break;
+            case 2:
+                g_board[0][2] = syb;
+                break;
+            case 3:
+                g_board[0][4] = syb;
+                break;
+
+            case 4:
+                g_board[2][0] = syb;
+                break;
+            case 5:
+                g_board[2][2] = syb;
+                break;
+            case 6:
+                g_board[2][4] = syb;
+                break;
+            case 7:
+                g_board[4][0] = syb;
+                break;
+            case 8:
+                g_board[4][2] = syb;
+                break;
+            case 9:
+                g_board[4][4] = syb;
+                break;
+            default:
+                System.out.println("Invalid Input ");
+        }
+        print_board(g_board);
     }
 }
